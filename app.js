82806a2bf147a968daaa0c9532fb85675e6fdec1
Cecilia <cecilia92fernandez@gmail.com>
2025-02-18 11:26:43 -0300

    se agrego slider y menu hamburguesa
    Falta darle funcionalidad al boton hamburguesa

---
+++ app.js
@@ -0,0 +1,24 @@
+let sliderContainer= document.querySelector(".slider-container");
+ let imagenes= sliderContainer.querySelectorAll("img");
+let index= 0;
+setInterval(() => {
+    let porcentaje=index*-100;
+    sliderContainer.style.transform= "translateX("+ porcentaje + "%)";
+    index++;
+    if(index >= imagenes.length){
+        index=0;
+    }
+},3000);
+
+//Menu hamburguesa
+const hamburguesa= document.querySelector('#menu-hamburguesa')
+const enlaces= document.querySelector('#nav-links')
+hamburguesa.addEventListener('click',()=>{
+    enlaces.classList.toggle('show');
+
+    if(enlaces.classList.contains('show')){
+        enlaces.style.display= 'flex';
+    }else{
+        enlaces.style.display='none';
+    }
+})
