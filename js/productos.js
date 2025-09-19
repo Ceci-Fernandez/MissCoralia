bb9a3c4372eff6d153b17b2f94f734ce36e5f36f
Cecilia <cecilia92fernandez@gmail.com>
2025-09-19 10:11:27 -0300

    Se genero el archivo js con productos

--- js/productos.js
+++ js/productos.js
@@ -1,21 +1,101 @@
 const productos = [
-    { id:1, img:"./img/accesorio/pexels-ashuphotography-1413420.jpg" ,nombre: "Aretes cascada turquesa", precio: 9000, tarjeta: "6 cuotas sin interés de $3.000", detalle: "Aretes largos con diseño en cascada y borlas de hilo turquesa. Cierre de gancho en metal dorado.",
-        colores: ["Turquesa", "Rojo", "Negro"],talles: [] },
-    { id:2, img:"./img/pantalones/pantalon_vestir.jpg" ,nombre: "pantalon ejecutivo milano", precio: 45000, tarjeta: "6 cuotas sin interés de $7.500", detalle: "Pantalón de vestir con corte recto y pinzas, confeccionado en tela sastrera.",
-        colores: ["Negro", "Gris", "Azul marino"],talles: ["S", "M", "L", "XL"] },
-    { id:3, img:"./img/remeras/remera rosa.jpg" ,nombre: "Remeron oversided-Rosa", precio: 20000, tarjeta: "6 cuotas sin interés de $3.333,34", detalle: "Remerón de algodón con corte oversized y mangas amplias.",
-        colores: ["Rosa pastel", "Lavanda", "Blanco"],talles: ["S", "M", "L"] },
-    { id:4, img:"img/pantalones/pexels-774990922-29503794.jpg" ,nombre: "Pantalón Wide Leg Urban", precio: 30000, tarjeta: "6 cuotas sin interés de $5.000",  detalle: "Pantalón de tiro alto y pierna ancha, en gabardina liviana.",
-        colores: ["Negro", "Beige"],talles: ["S", "M", "L", "XL"] },
-    { id:5, img:"./img/remeras/remera blanca.jpg" ,nombre: "Remera Blanca", precio: 54000, tarjeta: "6 cuotas sin interés de $9.000", detalle: "Remera de algodón premium con cuello redondo y corte holgado.",
-        colores: ["Blanco", "Negro", "Gris"],talles: ["XS", "S", "M", "L"] },
-    { id:6, img:"./img/remeras/remeron_floreado.jpg" ,nombre: "Vestido floreado", precio: 36000, tarjeta: "6 cuotas sin interés de $6.000", detalle: "Vestido corto con estampado floral vibrante, en gasa con forro interno.",
-        colores: ["Multicolor"],talles: ["S", "M", "L"] },
-    { id:7, img:"./img/pantalones/pexels-godisable-jacob-226636-970374 (1).jpg" ,nombre: "Conjunto Riviera Chic", precio: 40000, tarjeta: "6 cuotas sin interés de $6.666,67",detalle: "Conjunto de dos piezas: top de tirantes y pantalón jogger de lino.",
-        colores: ["Multicolor"],talles: ["S", "M", "L"] },
-    { id:8, img:"./img/accesorio/pexels-badis-benkhelil-1135505371-27835298.jpg" ,nombre: "Cartera de mano", precio: 50000, tarjeta: "6 cuotas sin interés de $8.833,33", detalle: "Cartera estructurada con solapa y cierre magnético en cuero ecológico.",
-        colores: ["Negro", "Beige", "Marrón"],talles: [] },
-    { id:9, img:"./img/pantalones/pexels-marcus-silva-86421404-16624071.jpg" ,nombre: "Pantalon de lino rosa", precio: 60000, tarjeta: "6 cuotas sin interés de $10.000,00", detalle: "Pantalón palazzo de lino, tiro alto con ajuste en la cintura.",
-        colores: ["Rosa", "Beige"],talles: ["S", "M", "L", "XL"] } 
+  {
+    id: 1,
+    categoria: "accesorios",
+    img: "/img/accesorio/pexels-ashuphotography-1413420.jpg",
+    nombre: "Aretes cascada turquesa",
+    precio: 9000,
+    tarjeta: "6 cuotas sin interés de $3.000",
+    detalle: "Aretes largos con diseño en cascada y borlas de hilo turquesa. Cierre de gancho en metal dorado.",
+    colores: ["#40e0d0", "#ff0000", "#000000"],
+    talles: []
+  },
+  {
+    id: 2,
+    categoria: "pantalones",
+    img: "/img/pantalones/pantalon_vestir.jpg",
+    nombre: "Pantalón ejecutivo Milano",
+    precio: 45000,
+    tarjeta: "6 cuotas sin interés de $7.500",
+    detalle: "Pantalón de vestir con corte recto y pinzas, confeccionado en tela sastrera.",
+    colores: ["#000000", "#808080", "#000080"],
+    talles: ["S", "M", "L", "XL"]
+  },
+  {
+    id: 3,
+    categoria: "remeras",
+    img: "/img/remeras/remera rosa.jpg",
+    nombre: "Remerón oversize - Rosa",
+    precio: 20000,
+    tarjeta: "6 cuotas sin interés de $3.333,34",
+    detalle: "Remerón de algodón con corte oversized y mangas amplias.",
+    colores: ["#f8c8dc", "#e6e6fa", "#ffffff"],
+    talles: ["S", "M", "L"]
+  },
+  {
+    id: 4,
+    categoria: "pantalones",
+    img: "img/pantalones/pexels-774990922-29503794.jpg",
+    nombre: "Pantalón Wide Leg Urban",
+    precio: 30000,
+    tarjeta: "6 cuotas sin interés de $5.000",
+    detalle: "Pantalón de tiro alto y pierna ancha, en gabardina liviana.",
+    colores: ["#000000", "#f5f5dc"],
+    talles: ["S", "M", "L", "XL"]
+  },
+  {
+    id: 5,
+    categoria: "remeras",
+    img: "/img/remeras/remera blanca.jpg",
+    nombre: "Remera Blanca",
+    precio: 54000,
+    tarjeta: "6 cuotas sin interés de $9.000",
+    detalle: "Remera de algodón premium con cuello redondo y corte holgado.",
+    colores: ["#ffffff", "#000000", "#808080"],
+    talles: ["XS", "S", "M", "L"]
+  },
+  {
+    id: 6,
+    categoria: "vestidos",
+    img: "/img/remeras/remeron_floreado.jpg",
+    nombre: "Vestido floreado",
+    precio: 36000,
+    tarjeta: "6 cuotas sin interés de $6.000",
+    detalle: "Vestido corto con estampado floral vibrante, en gasa con forro interno.",
+    colores: ["multicolor"],
+    talles: ["S", "M", "L"]
+  },
+  {
+    id: 7,
+    categoria: "conjuntos",
+    img: "/img/pantalones/pexels-godisable-jacob-226636-970374 (1).jpg",
+    nombre: "Conjunto Riviera Chic",
+    precio: 40000,
+    tarjeta: "6 cuotas sin interés de $6.666,67",
+    detalle: "Conjunto de dos piezas: top de tirantes y pantalón jogger de lino.",
+    colores: ["multicolor"],
+    talles: ["S", "M", "L"]
+  },
+  {
+    id: 8,
+    categoria: "accesorios",
+    img: "/img/accesorio/pexels-badis-benkhelil-1135505371-27835298.jpg",
+    nombre: "Cartera de mano",
+    precio: 50000,
+    tarjeta: "6 cuotas sin interés de $8.833,33",
+    detalle: "Cartera estructurada con solapa y cierre magnético en cuero ecológico.",
+    colores: ["#000000", "#f5f5dc", "#8b4513"],
+    talles: []
+  },
+  {
+    id: 9,
+    categoria: "pantalones",
+    img: "/img/pantalones/pexels-marcus-silva-86421404-16624071.jpg",
+    nombre: "Pantalón de lino rosa",
+    precio: 60000,
+    tarjeta: "6 cuotas sin interés de $10.000,00",
+    detalle: "Pantalón palazzo de lino, tiro alto con ajuste en la cintura.",
+    colores: ["#f8c8dc", "#f5f5dc"],
+    talles: ["S", "M", "L", "XL"]
+  }
 ];
-
